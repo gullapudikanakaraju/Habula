315c6a4378b5a4a1447ceb682c2acda387adb7f2
gullapudi kanakaraju <gullapudi kanakaraju>
2017-08-17 03:32:15 +0530

    added cse.html ece.html eee.html chemical.html mechanical.html and modified index.js file

--- index.js
+++ index.js
@@ -21,6 +21,26 @@ app.get('/profile',function(request,response){
   response.sendFile(__dirname+'/profile.html');
 });
 
+app.get('/cse',function(request,response){
+	response.sendFile(__dirname+'/cse.html');
+});
+
+app.get('/ece',function(request,response){
+	response.sendFile(__dirname+'/ece.html');
+});
+
+app.get('/eee',function(request,response){
+	response.sendFile(__dirname+'/eee.html');
+});
+
+app.get('/chemical',function(request,response){
+	response.sendFile(__dirname+'/chemical.html');
+});
+
+app.get('/mechanical',function(request,response){
+	response.sendFile(__dirname+'/mechanical.html');
+});
+
 app.listen(app.get('port'), function() {
   console.log('Node app is running on port', app.get('port'));
 });
